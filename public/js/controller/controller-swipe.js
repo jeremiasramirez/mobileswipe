3e1f632eba709a9c1129d585e1ac815a84457a6e
Jeremias ramirez <jereramirez499@gmail.com>
2019-10-31 19:40:26 -0400

    Mejorando estilos

--- public/js/controller/controller-swipe.js
+++ public/js/controller/controller-swipe.js
@@ -6,19 +6,22 @@ appMobileSwipe.controller("swipe", ["$scope", function($scope){
 
     $scope.images = ["image1", "image2", "image3", "image4", "image5"];
     $scope.img = $scope.images[0];
+    $scope.lenImg = $scope.images.length ;
 
     /*
     * counter of position -images
     * */
     $scope.counterImg = 0;
+    $scope.counterPosition = 1;
 
     // if swipe right
 
     $scope.swipeRight = function(){
 
 
-        if($scope.counterImg >= 1){
+        if($scope.counterImg > 0){
             $scope.counterImg -= 1;
+            $scope.counterPosition -= 1;
             $scope.img = $scope.images[$scope.counterImg];
         }
 
@@ -28,10 +31,12 @@ appMobileSwipe.controller("swipe", ["$scope", function($scope){
     //if swipe left
     $scope.swipeLeft = function(){
 
-        if($scope.counterImg < $scope.images.length){
+        if($scope.counterImg < $scope.images.length -1){
             $scope.counterImg += 1;
+            $scope.counterPosition +=1;
             $scope.img = $scope.images[$scope.counterImg];
         }
+
 
     }
 
