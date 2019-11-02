46e3f146eaa3dfa054045d7c36a8988b36e7835a
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-02 18:01:29 -0400

    Agregando funcionalidad de darle swipe hacia atras

--- public/js/controller/controller-swipe.js
+++ public/js/controller/controller-swipe.js
@@ -4,6 +4,13 @@
 
 appMobileSwipe.controller("swipe", ["$scope", function($scope){
 
+    //partials route
+
+    $scope.includeSwipeImg = "partials/partial-swipeimg.html";
+
+
+
+
     $scope.images = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9"];
     $scope.img = $scope.images[0];
     $scope.lenImg = $scope.images.length ;
@@ -25,9 +32,17 @@ appMobileSwipe.controller("swipe", ["$scope", function($scope){
     * */
             $scope.counterImg -= 1;
             $scope.counterPosition -= 1;
+
             $scope.img = $scope.images[$scope.counterImg];
 
         }
+        else if( ($scope.counterImg) < 1 ){
+
+            $scope.counterPosition = $scope.lenImg;
+            $scope.counterImg = $scope.lenImg - 1;
+            $scope.img = $scope.images[$scope.counterImg];
+        }
+
 
     };
 
@@ -41,6 +56,11 @@ appMobileSwipe.controller("swipe", ["$scope", function($scope){
             $scope.counterPosition +=1;
             $scope.img = $scope.images[$scope.counterImg];
 
+        }
+        else if ( ($scope.counterPosition >= $scope.lenImg ) ){
+            $scope.counterPosition = 1;
+            $scope.counterImg =0;
+            $scope.img = $scope.images[0];
         }
 
 
